1fda78b666351a56459bf73689fe0769cf85e5b1
mangaln <54597656+mangaln@users.noreply.github.com>
2019-09-24 21:29:16 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher Teacher = new Teacher { firstName = "John", lastName = "Doe" };
+            Console.WriteLine(Teacher.getInfo());
+        }
+    }
+    class Person
+    { public string firstName;
+        public string lastName;
+        public string getName()
+        {
+            return firstName + " " + lastName;
+        }
+    }
+    class Teacher:Person
+    {
+        public string IDnumber;
+        public string action;
+        public string subject;
+        public Teacher() : base()
+        {
+            IDnumber = "T12345";
+            action = "Teaches";
+            subject = "DIS";
+            
+        }
+        public string getInfo()
+        {
+            return IDnumber +" "+action+" "+subject+" " + base.getName();
+        }
+
+    }
+     
+}
+
